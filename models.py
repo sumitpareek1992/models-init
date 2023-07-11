73037591abb63e78d55076ddbd78e5fade6cb2ff
sumit <sumitsharma@topsinfosolutions.com>
2023-07-11 17:17:07 +0530

    model init

--- models.py
+++ models.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 class AccountUser(models.Model):
     first_name = models.CharField(max_length=200,null=True,blank=True)
     last_name = models.CharField(max_length=200,null=True,blank=True)
-    email = models.CharField(max_length=200,unique=True,null=True,blank=True)
+   
